cb18606b27789c5bd515efcc46d53f06f659f783
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2021-12-24 22:47:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppLesson17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+        void SetBankAccount <T> (BankAccount<T> account, T num, decimal chek, string[] name)
+        {
+            account.Num = num;
+
+        }
+    }
+
+    
+    class BankAccount<T>
+    {
+        private T Num { get; set; }
+        private decimal Chek { get; set; }
+        private string[] Name { get; set; } = new string[3];
+    }
+}
