a68febdd4f47b9427d5891eeeda6dab2fc3b372d
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2021-12-25 23:32:48 +0300

    Конечный вариант

--- ConsoleAppLesson17/Program.cs
+++ ConsoleAppLesson17/Program.cs
@@ -10,21 +10,58 @@ namespace ConsoleAppLesson17
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Номер счета типа int");
+            BankAccount<int> account = new BankAccount<int> ();
+            account.Num = 117;
+            account.Chek = 100000025;
+            account.Name[0] = "Иванов";
+            account.Name[1] = "Сергей";
+            account.Name[2] = "Иванович";
+            BankAccountPrint<int>(account);
+            Console.WriteLine();
+
+            Console.WriteLine("Номер счета типа string");
+            BankAccount<string> account1 = new BankAccount<string> ();
+            account1.Num = "е342то799";
+            account1.Chek = 100000025;
+            account1.Name[0] = "Не Иванов";
+            account1.Name[1] = "Не Сергей";
+            account1.Name[2] = "И не Ианович";
+            BankAccountPrint<string>(account1);
 
             Console.ReadKey();
         }
-        void SetBankAccount <T> (BankAccount<T> account, T num, decimal chek, string[] name)
+        static void BankAccountPrint<T> (BankAccount<T> account)
         {
-            account.Num = num;
-
+            Console.WriteLine("Номер банковского счета : {0}", account.Num);
+            Console.WriteLine("Состояние счета :  {0}", account.Chek);
+            Console.WriteLine("Владелец счета : {0} {1} {2}", account.Name[0], account.Name[1], account.Name[2]);
         }
+
     }
 
     
     class BankAccount<T>
     {
-        private T Num { get; set; }
-        private decimal Chek { get; set; }
-        private string[] Name { get; set; } = new string[3];
+        private T num;
+        private decimal chek;
+        private string[] name = new string[3];
+        
+        public T Num 
+        { 
+            get { return num; }
+            set { num = value; } 
+        }
+        public decimal Chek 
+        { 
+            get { return chek; }
+            set { chek = value; }
+        }
+        public string[] Name 
+        { 
+            get { return name; }
+            set { name = value; } 
+        } 
+
     }
 }
